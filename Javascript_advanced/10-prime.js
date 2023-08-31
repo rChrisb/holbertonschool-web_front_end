1d4d11aa9553c3204f0fdd595603a8c21eb1608e
rChrisb <5767@holbertonstudents.com>
2023-08-31 15:18:46 +0200

    Execution stack & timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,30 @@
+function isPrime(number) {
+  var count = 0;
+  for (var i = 1; i <= number; i++) {
+    if (number % i == 0) {
+      count = count + 1;
+    }
+  }
+  if (count == 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const countPrimeNumbers = function () {
+  let count = 0;
+  for (let i = 2; i < 101; i++) {
+    if (isPrime(i)) count += 1;
+  }
+  return count;
+};
+
+const startTime = performance.now();
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+const endTime = performance.now();
+const elapsedTime = endTime - startTime;
+console.log(
+  `Execution time of calculating prime numbers 100 times was ${elapsedTime} milliseconds`
+);
