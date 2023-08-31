99c28485ceeee038e457af246686bff763586b7e
rChrisb <5767@holbertonstudents.com>
2023-08-31 13:06:03 +0200

    closure

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,12 @@
+const welcomeMessage = function (fullName) {
+  alert(`Welcome ${fullName}`);
+};
+const guillaume = function () {
+  welcomeMessage("Guillaume");
+};
+const alex = function () {
+  welcomeMessage("Alex");
+};
+const fred = function () {
+  welcomeMessage("Fred");
+};
