168ff7f38bce143c23b4fb5e0aa4fcdfd2023d83
rChrisb <5767@holbertonstudents.com>
2023-08-31 15:09:26 +0200

    Prime numbers & timing execution

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,28 @@
+function isPrime(number) {
+  var count = 0;
+  for (var i = 1; i <= number; i++) {
+    if (number % i == 0) {
+      count = count + 1;
+    }
+  }
+  if (count == 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const startTime = performance.now();
+const countPrimeNumbers = function () {
+  let count = 0;
+  for (let i = 2; i < 101; i++) {
+    if (isPrime(i)) count += 1;
+  }
+  console.log(count);
+};
+
+countPrimeNumbers();
+const endTime = performance.now();
+const elapsedTime = endTime - startTime;
+console.log(
+  `Execution time of printing countPrimeNumbers was ${elapsedTime} milliseconds`
+);
